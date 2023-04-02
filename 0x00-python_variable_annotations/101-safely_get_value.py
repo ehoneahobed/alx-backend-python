b112dbc7ca62a991eda28a042013ca97666031ca
ehoneahobed <ehoneahobed@hotmail.com>
2023-04-02 20:40:47 +0000

    Update the function

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -21,6 +21,7 @@ T = typing.TypeVar('T')
 def safely_get_value(dct: typing.Mapping, key: typing.Any, default:
                      typing.Union[T, None] = None) -> \
         typing.Union[typing.Any, T]:
+    """annotations of the function"""
     if key in dct:
         return dct[key]
     else:
